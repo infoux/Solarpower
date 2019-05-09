82889a90bb10677b1ce7713e87295ba8df002d5e
rhkanrdl <emrdl7@gmail.com>
2019-05-09 11:13:08 +0900

    고도화 적용 및 WEB 분리

--- js/common.js
+++ js/common.js
@@ -1,57 +1,33 @@
 $(function() {
 
-    $(".gnb .menu").hide();
 
-    $(".gnb .menu_title").hover(function() {
-        $(this).children("ul").stop().slideToggle(250);
-    })
-
-
-
-    $(".logo i").click(function() {
-        $('body').scrollTop(0);
-        $("#gnb_bg").css("display", "block");
-        $('#gnb_bg').css("height", $(document).height());
-    });
-
-    $(".user_wrap i").click(function() {
-        $("#gnb_bg").css("display", "none");
-    });
-
-
-    $(window).on("resize", function() {
-
-        $("nav").removeAttr("style");
-    });
-
-    /* mobile(기술자 페이지) */
-
-    var gnb = $('.m_gnb');
+    var gnb = $('.gnb>li');
     var menu = $('.m_gnb_list ul');
 
     menu.hide();
 
+
+
     gnb.on({
-        mouseover: function() {
-            $(this).find('.m_gnb_list ul').stop()
-                .slideDown(200);
-        },
-        mouseout: function() {
-            $(this).find('.m_gnb_list ul').stop()
+        click: function() {
+            menu.stop()
                 .slideUp(200);
+
+            $(this).find(menu).stop()
+                .slideDown(200);
         }
     });
 
 
-    $(".m_title .icon-menu").click(function() {
+    $("header button.menu").click(function() {
         $('body').scrollTop(0);
-        $(".popup").css("display", "block");
-        $('.popup').css("height", $('#content').height());
-        $('.popup').css("width", $('#content').width());
-    });
+        $('nav.mainMenu').addClass("on");
 
-    $(".popup .icon-menu").click(function() {
-        $(".popup").css("display", "none");
+    });
+    $("nav.mainMenu button.closeMenu").click(function() {
+        $('body').scrollTop(0);
+        $('nav.mainMenu').removeClass("on");
+
     });
 
 });
